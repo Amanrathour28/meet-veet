9a8809e773fe26e1c416cb9f6cfcd99b806d76ce
parthdhanker <parthdhanker04@gmail.com>
2026-01-27 22:00:47 +0530

    tiny fixes

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,5 +1,6 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db";
+import User from "../models/User.js";
 
 
 
@@ -30,6 +31,7 @@ const deleteUserFromDB = inngest.createFunction(
     {id : "delete-user-from-db"},
     {event : "clerk/user.deleted"},
     async({event}) => {
+        await connectDB();
         const {id} = event.data;
         await User.deleteOne({clerkId : id});
     })
